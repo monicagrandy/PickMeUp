dcc4050517d3fce27631ed9836a404e766b3b4f7
Monica Grandy <monica.grandy@gmail.com>
2016-02-16 15:04:40 -0800

    updated server, package.jason, css

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var cors = require('cors');
-var port = 8080;
+var port = process.env.PORT || 8080;
 app.use(morgan('tiny'));
+
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/client/'));
 app.use('/node_modules', express.static(__dirname + '/node_modules/'));
+
+app.get('/', function(req,res){
+  res.render('index');
+})
 
 app.listen(port);
 console.log("Express server listening on %d in %s mode", port, app.settings.env);
